5a6bddf6d26f8f4466d36557f8ac20bca4e6f166
Sean Lo <seanlo@mit.edu>
2025-03-20 12:14:56 -0700

    update experiments.jl - remove code at end

--- models/experiment.jl
+++ models/experiment.jl
@@ -28,6 +28,8 @@ function run_experiment(
     demand_fp::String,
     result_dir::String,
     ;
+    write::Bool = true,
+    env::Union{Gurobi.Env, Nothing} = nothing,
     strategy::String = "SSOA",
     discount_factor::Float64 = 0.1,
     use_batching::Bool = false,
@@ -35,8 +37,12 @@ function run_experiment(
     online_objectives::Bool = true,
     S::Int = 1,
     seed::Int = 0,
+    MIPGap::Float64 = 1e-4,
     time_limit_sec_per_iteration = 300,
 )
+    if isnothing(env)
+        env = Gurobi.Env()
+    end
     RCoeffs = RackPlacementCoefficients(
         discount_factor = discount_factor,
     )
@@ -46,89 +52,122 @@ function run_experiment(
     batches, batch_sizes = read_demand(demand_fp, RCoeffs, use_batching, batch_size)
     
     if strategy == "oracle"
-        oracle_result = rack_placement_oracle(batches, batch_sizes, DC)
+        result = rack_placement_oracle(batches, batch_sizes, DC, env, Inf)
     elseif strategy == "myopic"
-        myopic_result = rack_placement(
-            DC, Sim, RCoeffs, batches, batch_sizes, strategy = "myopic",
+        result = rack_placement(
+            DC, Sim, RCoeffs, batches, batch_sizes, 
+            env = env,
+            strategy = "myopic",
             time_limit_sec_per_iteration = time_limit_sec_per_iteration,
+            obj_minimize_rooms = online_objectives,
+            obj_minimize_rows = online_objectives,
+            obj_minimize_tilegroups = online_objectives,
+            obj_minimize_power_surplus = online_objectives,
+            obj_minimize_power_balance = online_objectives,
+            MIPGap = MIPGap,
         )
-        r = postprocess_results(myopic_result["all_results"], DC, "myopic")
+        r = postprocess_results(
+            result["all_results"], DC, "myopic";
+            obj_minimize_rooms = online_objectives,
+            obj_minimize_rows = online_objectives,
+            obj_minimize_tilegroups = online_objectives,
+            obj_minimize_power_surplus = online_objectives,
+            obj_minimize_power_balance = online_objectives,
+        )
     elseif strategy == "MPC"
-        MPC_result = rack_placement(
-            DC, Sim, RCoeffs, batches, batch_sizes, strategy = "MPC",
+        result = rack_placement(
+            DC, Sim, RCoeffs, batches, batch_sizes, 
+            env = env,
+            strategy = "MPC",
             time_limit_sec_per_iteration = time_limit_sec_per_iteration,
             obj_minimize_rooms = online_objectives,
             obj_minimize_rows = online_objectives,
             obj_minimize_tilegroups = online_objectives,
             obj_minimize_power_surplus = online_objectives,
             obj_minimize_power_balance = online_objectives,
+            MIPGap = MIPGap,
         )
-        r = postprocess_results(MPC_result["all_results"], DC, "MPC")
+        r = postprocess_results(
+            result["all_results"], DC, "MPC";
+            obj_minimize_rooms = online_objectives,
+            obj_minimize_rows = online_objectives,
+            obj_minimize_tilegroups = online_objectives,
+            obj_minimize_power_surplus = online_objectives,
+            obj_minimize_power_balance = online_objectives,
+        )
     elseif strategy == "SSOA"
-        SSOA_result = rack_placement(
-            DC, Sim, RCoeffs, batches, batch_sizes, strategy = "SSOA", S = 1, seed = seed,
+        result = rack_placement(
+            DC, Sim, RCoeffs, batches, batch_sizes, 
+            env = env,
+            strategy = "SSOA", S = 1, seed = seed,
             time_limit_sec_per_iteration = time_limit_sec_per_iteration,
             obj_minimize_rooms = online_objectives,
             obj_minimize_rows = online_objectives,
             obj_minimize_tilegroups = online_objectives,
             obj_minimize_power_surplus = online_objectives,
             obj_minimize_power_balance = online_objectives,
+            MIPGap = MIPGap,
         )
-        r = postprocess_results(SSOA_result["all_results"], DC, "SSOA")
+        r = postprocess_results(
+            result["all_results"], DC, "SSOA";
+            obj_minimize_rooms = online_objectives,
+            obj_minimize_rows = online_objectives,
+            obj_minimize_tilegroups = online_objectives,
+            obj_minimize_power_surplus = online_objectives,
+            obj_minimize_power_balance = online_objectives,
+        )
     elseif strategy == "SAA"
-        SAA_result = rack_placement(
-            DC, Sim, RCoeffs, batches, batch_sizes, strategy = "SAA", S = S, seed = seed,
+        result = rack_placement(
+            DC, Sim, RCoeffs, batches, batch_sizes, 
+            env = env,
+            strategy = "SAA", S = S, seed = seed,
             time_limit_sec_per_iteration = time_limit_sec_per_iteration,
             obj_minimize_rooms = online_objectives,
             obj_minimize_rows = online_objectives,
             obj_minimize_tilegroups = online_objectives,
             obj_minimize_power_surplus = online_objectives,
             obj_minimize_power_balance = online_objectives,
+            MIPGap = MIPGap,
         )
-        r = postprocess_results(SAA_result["all_results"], DC, "SAA")
+        r = postprocess_results(
+            result["all_results"], DC, "SAA";
+            obj_minimize_rooms = online_objectives,
+            obj_minimize_rows = online_objectives,
+            obj_minimize_tilegroups = online_objectives,
+            obj_minimize_power_surplus = online_objectives,
+            obj_minimize_power_balance = online_objectives,
+        )
+    end
+
+    if write 
+        if strategy != "oracle"
+            CSV.write("$(result_dir)/iteration_data.csv", r["iteration_data"])
+            CSV.write("$(result_dir)/room_space_utilization.csv", r["room_space_utilization_data"])
+            if online_objectives
+                CSV.write("$(result_dir)/toppower_utilization.csv", r["toppower_utilization_data"])
+                CSV.write("$(result_dir)/toppower_pair_utilization.csv", r["toppower_pair_utilization_data"])
+            end
+        else
+            CSV.write("$(result_dir)/objective.csv", DataFrame(Dict("objective" => [result["objective"]])))
+        end
     end
 
     if strategy != "oracle"
-        CSV.write("$(result_dir)/$(strategy).csv", r["iteration_data"])
-        CSV.write("$(result_dir)/$(strategy)_room_space_utilization.csv", r["room_space_utilization_data"])
-        if online_objectives
-            CSV.write("$(result_dir)/$(strategy)_toppower_utilization.csv", r["toppower_utilization_data"])
-            CSV.write("$(result_dir)/$(strategy)_toppower_pair_utilization.csv", r["toppower_pair_utilization_data"])
-        end
+        optimality_gap_vals = r["iteration_data"][!, "optimality_gap"]
+        optimality_gap_mean = StatsBase.geomean(optimality_gap_vals .+ 1.0) - 1.0
+        returnval = Dict(
+            "time_taken" => result["time_taken"],
+            "optimality_gap_mean" => optimality_gap_mean,
+            "demands_placed" => Int(round((r["iteration_data"][!, "current_assignment"] |> sum) / (RCoeffs.placement_reward))),
+        )
     else
-        CSV.write("$(result_dir)/$(strategy).csv", DataFrame(Dict("objective" => [oracle_result["objective"]])))
+        optimality_gap_mean = 0.0
+        returnval = Dict(
+            "time_taken" => result["time_taken"],
+            "optimality_gap_mean" => optimality_gap_mean,
+            "demands_placed" => Int(round(result["objective"] / (RCoeffs.placement_reward))),
+        )
     end
-    return
-end
 
-
-### Filepaths ### 
-fps = JSON.parsefile("$(@__DIR__)/../filepaths.json")
-datacenter_dir = fps["datacenter_dir"]
-distr_dir = fps["distr_dir"]
-demand_fp = fps["demand_fp"]
-### 
-
-for (strategy, discount_factor, online_objectives, seed) in Iterators.product(
-    ["oracle", "myopic", "SSOA", "MPC", "SAA"],
-    [0.1, 0.5, 1.0],
-    [true, false],
-    [1, 2, 3, 4, 5],
-)
-    result_dir = "$(@__DIR__)/../experiments/$(discount_factor)_$(online_objectives)_$(seed)"
-    if !isdir(result_dir)
-        mkdir(result_dir)
-    end
-    run_experiment(
-        datacenter_dir,
-        distr_dir,
-        demand_fp,
-        result_dir,
-        ;
-        strategy = strategy,
-        discount_factor = discount_factor,
-        time_limit_sec_per_iteration = 20,
-        online_objectives = online_objectives,
-        seed = seed,
-    )
+    return returnval
 end
