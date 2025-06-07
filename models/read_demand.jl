96f7258c08cb7ed0dd7867cd07f985c3c91a776d
Sean Lo <seanlo@mit.edu>
2025-06-07 15:21:54 -0700

    add rounding and discretization in read_demand()

--- models/read_demand.jl
+++ models/read_demand.jl
@@ -7,6 +7,11 @@ function read_demand(
     placement_var_reward::Float64 = 0.0,
     use_batching::Bool = false,
     batch_size::Int = CONST_BATCH_SIZE,
+    ;
+    round_cooling::Bool = false,
+    round_power::Bool = false,
+    cooling_discretization::Float64 = 1.0,
+    power_discretization::Float64 = 1.0,
 )
     demand_data = CSV.read(demand_fp, DataFrame)
     if use_batching && (:batchID in names(demand_data))
@@ -14,10 +19,18 @@ function read_demand(
         batches = Dict{String, <:Array}[]
         for t in 1:T
             sizes = demand_data[demand_data[!, :batchID] .== t, :size]
+            cooling_vals = demand_data[demand_data[!, :batchID] .== t, :coolingEach]
+            power_vals = demand_data[demand_data[!, :batchID] .== t, :powerEach]
+            if round_cooling
+                cooling_vals = round.(cooling_vals / cooling_discretization) * cooling_discretization
+            end
+            if round_power
+                power_vals = round.(power_vals / power_discretization) * power_discretization
+            end
             push!(batches, Dict(
                 "size" => sizes,
-                "cooling" => demand_data[demand_data[!, :batchID] .== t, :coolingEach],
-                "power" => demand_data[demand_data[!, :batchID] .== t, :powerEach],
+                "cooling" => cooling_vals,
+                "power" => power_vals,
                 "reward" => (placement_reward .+ placement_var_reward .* sizes),
             ))
         end
@@ -26,10 +39,18 @@ function read_demand(
         batches = Dict{String, <:Array}[]
         for inds in Iterators.partition(1:nrow(demand_data), batch_size)
             sizes = demand_data[inds, :size]
+            cooling_vals = demand_data[inds, :coolingEach]
+            power_vals = demand_data[inds, :powerEach]
+            if round_cooling
+                cooling_vals = round.(cooling_vals / cooling_discretization) * cooling_discretization
+            end
+            if round_power
+                power_vals = round.(power_vals / power_discretization) * power_discretization
+            end
             push!(batches, Dict(
                 "size" => sizes,
-                "cooling" => demand_data[inds, :coolingEach],
-                "power" => demand_data[inds, :powerEach],
+                "cooling" => cooling_vals,
+                "power" => power_vals,
                 "reward" => (placement_reward .+ placement_var_reward .* sizes),
             ))
         end
